f96694411a3a708abc202c97aea7a2df49c353b7
Tim Leach <timleach635@gmail.com>
2021-09-08 12:32:06 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.scss';
+import "./App.scss";
 import {
   BrowserRouter as Router,
   Switch,
   Route
-} from 'react-router-dom';
-import { PostListPage } from './components/PostListPage';
+} from "react-router-dom";
+import { Header } from "./components/header/Header";
+import { PostListPage } from "./components/PostListPage";
 import { UserDetailPage } from "./components/UserDetailPage";
 
 function App() {
   return <Router>
+    <Header />
     <main>
       <h1>MyFace</h1>
       <Switch>
